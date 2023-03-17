3218f86a965fb0386704b4bd838003e089afb2e4
Pratik Deshmukh <86339747+pratikdeshmukh570@users.noreply.github.com>
2023-03-17 16:59:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# Provider Configuration
 resource "aws_key_pair" "key" {
   key_name   = "pratik"
   public_key = file("~/.ssh/aws.pub")
@@ -11,14 +10,11 @@ resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
 
-  # using default VPC
+ 
   vpc_id      = aws_default_vpc.default_vpc.id
 
   ingress {
     description = "TLS from VPC"
-
-    # we should allow incoming and outoging
-    # TCP packets
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -35,10 +31,10 @@ resource "aws_instance" "ec2_instance" {
   ami             = var.ami_id
   instance_type   = "t2.micro"
 
-  # refering key which we created earlier
+ 
   key_name        = aws_key_pair.key.key_name
 
-  # refering security group created earlier
+ 
   security_groups = [aws_security_group.allow_ssh.name]
 
   tags = var.tags
@@ -48,7 +44,7 @@ resource "aws_instance" "ec2_instance" {
 variable "ami_id" {
   description = "Ubuntu ami id"
 
-  # Amazon linux image
+
   default     = "ami-0caf778a172362f1c"
 }
 
